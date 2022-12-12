b6e0f775076eff33fd8dbbaaec20f2851163c0e2
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2022-12-12 10:07:24 +0100

    Updated script and included description of generation process

--- human_readable_generator/generate.sh
+++ human_readable_generator/generate.sh
@@ -2,7 +2,10 @@
 
 output=$(cat template.md)
 
-platforms_table=$(queryRdfFile  -of simple "../data/platforms.ttl" ./list-platform.sparql)
+platforms_table=$(queryRdfFile "../data/platforms.ttl" ../queries/list-platform.sparql)
 output="${output//%%PLATFORMS_TABLE%%/$platforms_table}"
+
+projects_table=$(queryRdfFile "../data/projects.ttl" ../queries/list-projects.sparql)
+output="${output//%%PROJECTS_TABLE%%/$projects_table}"
 
 echo "$output" > ../inventory.md
