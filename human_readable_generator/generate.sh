65e151d909f92df5fb331e4df39a3cec52d3c926
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-09-10 15:24:14 +0200

    Updated bash script regarding newlines

--- human_readable_generator/generate.sh
+++ human_readable_generator/generate.sh
@@ -4,12 +4,16 @@ output=$(cat template.md)
 
 table=$(queryRdfFile "../data/*.ttl" ../queries/list-platform.sparql)
 count=$(( $(echo "$table" | wc -l) -2))
-table="${table}\n\nCount: ${count}"
-output="${output//%%PLATFORMS_TABLE%%/$table}"
+table_info="${table}"
+table_info+=$'\n\n'
+table_info+="Count: ${count}"
+output="${output//%%PLATFORMS_TABLE%%/$table_info}"
 
 table=$(queryRdfFile "../data/*.ttl" ../queries/list-projects.sparql)
 count=$(( $(echo "$table" | wc -l) -2))
-table="${table}\n\nCount: ${count}"
-output="${output//%%PROJECTS_TABLE%%/$table}"
+table_info="${table}"
+table_info+=$'\n\n'
+table_info+="Count: ${count}"
+output="${output//%%PROJECTS_TABLE%%/$table_info}"
 
 echo "$output" > ../docs/inventory.md
